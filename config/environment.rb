19662da5bca2e866db4a937e3c8849ee9d5edb92
Saloni <salonimehta2127@gmail.com>
2021-09-16 16:05:24 -0400

    Completed

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,8 @@
 # Set up Bundler here following the instructions in the README
+#this code
+require 'bundler/setup'
+Bundler.require(:default, :development)
+#is equivalent to
+# require 'rspec'
+# require 'rest-client'
+# require 'pry'
